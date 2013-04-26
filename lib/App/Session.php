4c2e405087a127c3344f0d11c94991d2d853f7ab
criztype <cgellhorn@pcisa-181.isa-hamburg.lan>
2013-04-26 22:40:31 +0200

    Added remember me seconds

--- lib/App/Session.php
+++ lib/App/Session.php
@@ -7,6 +7,14 @@
  */
 class App_Session
 {
+	/**
+     * Default number of seconds the session will be remembered 
+	 * for when asked to be remembered
+     *
+     * @var int
+     */
+    private static $_rememberMeSeconds = 1209600; // 2 weeks
+	
 	public function setStorage()
 	{
 		
