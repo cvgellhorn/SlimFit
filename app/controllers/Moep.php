e187afbc049d7c4e245623c7ea0ccc498023f73f
criztype <cgellhorn@pcisa-181.isa-hamburg.lan>
2013-04-26 22:47:14 +0200

    Edit index and test action name

--- app/controllers/Moep.php
+++ app/controllers/Moep.php
@@ -7,14 +7,29 @@
  */
 class Moep extends App_Controller
 {	
-	public function indexAct()
+	public function indexAction($test)
 	{
+		App_Debug::dump($this->request->getParam('id'));
+		
+		App_Debug::dump($test);
 		App_Debug::dump('INDEX ACTION');
 	}
 	
-	public function testAct($args)
+	public function testAction($args)
 	{
 		$this->view->teeeeestValue = '999999999999999';
+		
+		$adapter = App_Auth::getInstance()->getAdapter();
+		$adapter->setLoginData(array(
+			'username'	=> 'test',
+			'password'	=> 'dhdhdhh4df4fdj24'
+		));
+		
+		try {
+			$adapter->login();
+		} catch (App_Auth_Adapter_Exception $e) {
+			//-- Perform something
+		}
 		
 		/*App_Debug::dump('arguments: ');
 		App_Debug::dump($args);
