1eddcc29f1aceaf2d75dbe342ca0796680684c64
criztype <cgellhorn@pcisa-181.isa-hamburg.lan>
2013-04-26 22:26:13 +0200

    Added user info getters

--- lib/App/Auth/Identity.php
+++ lib/App/Auth/Identity.php
@@ -5,7 +5,37 @@
  *
  * @author cvgellhorn
  */
-class App_Auth_Identity
+class App_Auth_Identity extends App_Model
 {
-
+	/**
+	 * Get the current username
+	 */
+	public function getUsername()
+	{
+		return $this->getData('username');
+	}
+	
+	/**
+	 * Get the current user email
+	 */
+	public function getEmail()
+	{
+		return $this->getData('email');
+	}
+	
+	/**
+	 * Get the current user firstname
+	 */
+	public function getFirstname()
+	{
+		return $this->getData('firstname');
+	}
+	
+	/**
+	 * Get the current user lastname
+	 */
+	public function getLastname()
+	{
+		return $this->getData('lastname');
+	}
 }
