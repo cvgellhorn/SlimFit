1627464095c9754b236c6f788855fa6fabfb1f47
cvgellhorn <Chris@ichris.fritz.box>
2014-05-04 22:50:49 +0200

    Changed comment style

--- public/index.php
+++ public/index.php
@@ -1,24 +1,24 @@
 <?php
 
-//-- Define directory separator
+// Define directory separator
 defined('DS') || define('DS', DIRECTORY_SEPARATOR);
 
-//-- Define path to application directory
+// Define path to application directory
 defined('APP_PATH') || define('APP_PATH',
               realpath(dirname(__FILE__) . DS . '..' . DS . 'app'));
  
-//-- Define application environment
+// Define application environment
 defined('APP_ENV') || define('APP_ENV',
               (getenv('APP_ENV') ? getenv('APP_ENV') : 'production'));
 
-//-- Set include path
+// Set include path
 set_include_path(implode(PATH_SEPARATOR, array(
     dirname(dirname(__FILE__)) . '/lib',
     get_include_path(),
 )));
 
-//-- Include main app class
+// Include main app class
 require_once 'App.php';
 
-//-- Run Application
+// Run Application
 App::run();
