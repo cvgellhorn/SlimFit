ce7542a6d428b6c2c27e91fdbfdbdf9985e40780
cvgellhorn <gellborn@gmail.com>
2014-09-20 23:25:59 +0200

    Rebuild index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Define directory separator alias
+defined('DS') || define('DS', DIRECTORY_SEPARATOR);
+
+// Define application main directories
+defined('BASE_DIR') || define('BASE_DIR', __DIR__ . DS);
+define('APP_DIR', BASE_DIR . 'app');
+
+// Define application environment
+defined('APP_ENV') || define('APP_ENV',
+	(getenv('APP_ENV') ? getenv('APP_ENV') : 'production'));
+
+// Set include path
+set_include_path(implode(PATH_SEPARATOR, array(
+	BASE_DIR . 'lib',
+    get_include_path(),
+)));
+
+// Register autoloader
+require_once('SF/Autoloader.php');
+SF\Autoloader::register();
+
+// Run SlimFit application
+SF::run();
