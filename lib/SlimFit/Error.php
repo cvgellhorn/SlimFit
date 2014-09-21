16d4d98216af2b734539654d41fca5b222fe0b14
cvgellhorn <gellborn@gmail.com>
2014-09-21 02:03:21 +0200

    Class refactored

---
+++ lib/SlimFit/Error.php
@@ -0,0 +1,24 @@
+<?php namespace SlimFit;
+
+use Exception;
+use SF;
+
+/**
+ * Main exception class
+ * 
+ * @author cvgellhorn
+ */
+class Error extends Exception
+{
+	/**
+	 * Constructor of exceptions class
+	 * 
+	 * @param string @message Exception message
+	 * @param int @code Exception code
+	 */
+	public function __construct($message = null, $code = null) 
+	{
+		parent::__construct($message, $code);
+		SF::log($this, SF::LOG_FILE_ERROR);
+	}
+}
