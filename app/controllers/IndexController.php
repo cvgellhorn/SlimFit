2b4a8c1e7f59936458a6cb7fb840f2196ceb0787
cvgellhorn <gellborn@gmail.com>
2014-09-21 19:42:17 +0200

    Class refactored

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,43 @@
+<?php
+
+use SlimFit\Controller;
+use SlimFit\Debug;
+
+/**
+ * Index Controller
+ *
+ * @author cvgellhorn
+ */
+class Index extends Controller
+{
+	public function indexAction()
+	{
+		//$this->redirect('moep/index');
+		$db = SF_Db::getInstance();
+		$result = $db->fetchAssoc('SELECT * FROM `data` WHERE `test` = \'blub\'', 'sdsdh');
+		Debug::dump($result);
+
+		/*App_Debug::dump($db->insert('data', array(
+			'test' => 'nevermind',
+			'moep' => 'test',
+			'value' => new App_Db_Expr('NOW()')
+		)));*/
+
+		/*App_Debug::dump($db->query("INSERT INTO `data` (moep, test, value) VALUES ('blub', 'qwerty', 'h eheh erh erher')"));
+		//App_Debug::dump($db->query("UPDATE `data` SET moep = 'teeeer', `value` = 'dfsdfsdgs' WHERE moep = 'check' AND `value` = 'blaaaaa'"));
+		App_Debug::dump($db->query("SELECT moep FROM data WHERE moep = 'teeeer'"));
+		App_Debug::dump('-------------------------------------');
+		App_Debug::dump($db->query("SELECT * FROM data WHERE moep = 'teeeer'"));
+		App_Debug::dump('-------------------------------------');
+		App_Debug::dump($db->query("SELECT * FROM data"));
+		App_Debug::dump('-------------------------------------');
+		App_Debug::dump($db->query("SELECT * FROM data WHERE moep = 'sdhgfsdhgjksdhgjsdhjksghjk'"));*/
+
+		Debug::dump($db->fetchAll("SEELECT * FROM data WHERE moep = 'sdhgfsdhgjksdhgjsdhjksghjk'"));
+
+		/*App_Debug::dump($db->delete('data', array(
+			'test = ?' => 'chingchang',
+			'moep = ?' => new App_Db_Expr('NOW()')
+		)));*/
+	}
+}
