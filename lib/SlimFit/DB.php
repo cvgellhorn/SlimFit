0dbd5ba940b8885c85e3807265d11cd15e238f98
cvgellhorn <gellborn@gmail.com>
2014-09-26 19:15:12 +0200

    Edit DB class

---
+++ lib/SlimFit/DB.php
@@ -0,0 +1,14 @@
+<?php namespace SlimFit;
+
+/**
+ * SlimFit Database class
+ *
+ * Using the OneDB framework
+ * https://github.com/cvgellhorn/OneDB
+ *
+ * @author cvgellhorn
+ */
+class DB
+{
+
+}
